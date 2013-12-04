f3c250c2553ff82bb26d0f4176ce677a5ee3b3f7
Dae-seon Moon <smoon@parrotmarketing.com>
2013-12-04 12:14:25 -0500

    add pymongo ane flask-pymongo

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='YourAppName', version='1.0',
+setup(name='engfordev', version='1.0',
       description='OpenShift Python-2.7 Community Cartridge based application',
-      author='Your Name', author_email='ramr@example.org',
+      author='Sean Moon', author_email='daeseonmoon@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'gevent', 'Flask'
+      install_requires=['greenlet', 'gevent', 'Flask','pymongo','Flask-Pymongo'
                         #  'MySQL-python',
                         #  'pymongo',
                         #  'psycopg2',
