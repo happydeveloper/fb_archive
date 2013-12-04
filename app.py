f089f204f1fa3ee89513050b3dd981262ec039b6
Dae-seon Moon <smoon@parrotmarketing.com>
2013-12-04 13:59:38 -0500

    remove feed code

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ def get_access_token():
 	app_secret = "aba7af8db27670642efb196ab968ce42"
 	group_id = "157076174344216"
 	token = facebook.get_app_access_token(app_id,app_secret)
-	response = urllib2.urlopen("https://graph.facebook.com/" + group_id + "?fields=feed&method=GET&format=json&suppress_http_code=1&access_token=" + str(token))
-	feed = json.loads(response.read())
+	print token
+	#response = urllib2.urlopen("https://graph.facebook.com/" + group_id + "?fields=feed&method=GET&format=json&suppress_http_code=1&access_token=" + str(token))
+	#feed = json.loads(response.read())
 	return feed
 
 
