4e28ca3c7badd9b62d6ad6645b035e8f8a0d6ff2
Dae-seon Moon <deaseonmoon@gmail.com>
2014-01-06 16:08:41 -0500

    fix cron path

--- .openshift/cron/daily/fetch.sh
+++ .openshift/cron/daily/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'fetching data'
-source ${OPENSHIFT_HOMEDIR}/python/bin/activate_virtenv
-python ${OPENSHIFT_HOMEDIR}/fetch.py
+source ${OPENSHIFT_HOMEDIR}python/bin/activate_virtenv
+python ${OPENSHIFT_HOMEDIR}app-root/repo/fetch.py
 echo 'end fetching'
