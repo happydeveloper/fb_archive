798696f1094315b0c0dad8cc502bc705019df1b8
Dae-seon Moon <deaseonmoon@gmail.com>
2014-01-06 16:19:55 -0500

    update cron script path

--- .openshift/cron/daily/fetch.sh
+++ .openshift/cron/daily/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'fetching data'
-source ${OPENSHIFT_HOMEDIR}python/bin/activate_virtenv
+source ${OPENSHIFT_HOMEDIR}python/virtualenv/bin/activate
 python ${OPENSHIFT_HOMEDIR}app-root/repo/fetch.py
 echo 'end fetching'
