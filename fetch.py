82b8597a418db91b251ba4c84016522d0fc42a68
Dae-seon Moon <deaseonmoon@gmail.com>
2014-01-10 15:29:55 -0500

    Implementing indexing

--- fetch.py
+++ fetch.py
@@ -3,15 +3,55 @@ import urllib2
 import config
 import json
 import facebook
-from pymongo import MongoClient
+import time
+import re
 
+from pymongo import MongoClient
+#conn = "mongodb://localhost:27017/engfordev"
+#client = MongoClient(conn)
 conn = "mongodb://" + config.MONGO_USERNAME + ":" + config.MONGO_PASSWORD + "@" + config.MONGO_HOST + ":" + config.MONGO_PORT + "/engfordev"
 client = MongoClient(conn)
 token = config.ACCESS_TOKEN
+#print "https://graph.facebook.com/" + config.GROUPS["engfordev"] + "?fields=feed&method=GET&format=json&suppress_http_code=1&access_token=" + str(token)
 response = urllib2.urlopen("https://graph.facebook.com/" + config.GROUPS["engfordev"] + "?fields=feed&method=GET&format=json&suppress_http_code=1&access_token=" + str(token))
 data = json.loads(response.read())
-articles = []
-print data
+
+if "error" in data:
+	if data["error"]["code"] == 613:
+		time.sleep(600)
+		response = urllib2.urlopen("https://graph.facebook.com/" + config.GROUPS["engfordev"] + "?fields=feed&method=GET&format=json&suppress_http_code=1&access_token=" + str(token))
+		data = json.loads(response.read())
+
+prev = data["feed"]["paging"]["next"]
+feed = data["feed"]
+col = client.engfordev.feed
+col.drop()
+col.insert(feed["data"])
+total_request = 1
+while prev:
+	total_request += 1
+	try:
+		response = urllib2.urlopen(prev)
+		data = json.loads(response.read())
+		if len(data["data"]) > 0:
+			col.insert(data["data"])
+		if "paging" in data and "next" in data["paging"]:
+			prev = data["paging"]["next"]
+		else:
+			prev = False
+	except urllib2.HTTPError:
+		print "Request error: [Url:" + prev + "]\n"
+		time.sleep(60)
+
+print "Total Request: [" + str(total_request) + "]"
+
+feed = col.find()
+hash_reg = re.compile("[#]\S+",re.UNICODE)
+for f in feed:
+	if "message" in f:
+		tag = hash_reg.findall(f["message"])
+		if len(tag) > 0:
+			col.update({"_id":f["_id"]},{'$set':{"hashtags":tag}},upsert=True)
 '''
 feed = data["feed"]
 for f in feed["data"]:
