881b4194dc8104dcbd2cd0f0ec70c51d6831d633
Dae-seon Moon <deaseonmoon@gmail.com>
2014-01-13 11:42:54 -0500

    add debug code

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ mongo = PyMongo(myapp)
 
 @myapp.template_filter('urlencode')
 def urlencode(uri, **query):
-	print uri
+	print uri.encode('utf8')
 	parts = list(urlparse.urlparse(uri))
 	q = urlparse.parse_qs(parts[4])
 	q.update(query)
