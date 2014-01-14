389fdd1dc2682f5d07388df642dda21e55fda1ee
Dae-seon Moon <deaseonmoon@gmail.com>
2014-01-14 14:04:26 -0500

    add htmlentitydecode in filter

--- app/filters/__init__.py
+++ app/filters/__init__.py
@@ -1,9 +1,11 @@
 import urllib
 import re
+import HTMLParser
 from jinja2 import evalcontextfilter, Markup, escape
 from app import engfordev
 
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+parser = HTMLParser.HTMLParser()
 
 @engfordev.template_filter('nl2br')
 @evalcontextfilter
@@ -17,5 +19,12 @@ def nl2br(eval_ctx, value):
 def urlencode(uri, **query):
 	return urllib.quote_plus(uri.encode("utf8"))
 
+@engfordev.template_filter('htmlentitydecode')
+@evalcontextfilter
+def htmlentitydecode(eval_ctx, value):
+	return parser.unescape(value)
+
+
 engfordev.jinja_env.globals['urlencode'] = urlencode
 engfordev.jinja_env.globals['nl2br'] = nl2br
+engfordev.jinja_env.globals['htmlentitydecode'] = htmlentitydecode
