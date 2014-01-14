1865eab38407a5f03df3286d38db837223aa68de
Dae-seon Moon <deaseonmoon@gmail.com>
2014-01-14 14:07:28 -0500

    update conn str

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from app import engfordev
 
 logging.basicConfig(stream=sys.stderr)
 
-'''
 PYCART_DIR = ''.join(['python-', '.'.join(map(str, sys.version_info[:2]))])
 
 try:
@@ -26,7 +25,6 @@ def run_gevent_server(app, ip, port=8080):
 def run_simple_httpd_server(app, ip, port=8080):
    from wsgiref.simple_server import make_server
    make_server(ip, port, app).serve_forever()
-'''
 
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
@@ -38,14 +36,14 @@ def run_simple_httpd_server(app, ip, port=8080):
 #  main():
 #
 if __name__ == '__main__':
-	engfordev.run()
-	#ip   = os.environ['OPENSHIFT_PYTHON_IP']
-	#port = 8080
-	#zapp = imp.load_source('application', 'wsgi/application')
-	##  Use gevent if we have it, otherwise run a simple httpd server.
-	#print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
-	#try:
-	#	run_gevent_server(engfordev, ip, port)
-	#except:
-	#	print 'gevent probably not installed - using default simple server ...'
-	#	run_simple_httpd_server(zapp.application, ip, port)
+	#engfordev.run()
+	ip   = os.environ['OPENSHIFT_PYTHON_IP']
+	port = 8080
+	zapp = imp.load_source('application', 'wsgi/application')
+	#  Use gevent if we have it, otherwise run a simple httpd server.
+	print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
+	try:
+		run_gevent_server(engfordev, ip, port)
+	except:
+		print 'gevent probably not installed - using default simple server ...'
+		run_simple_httpd_server(zapp.application, ip, port)
--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ engfordev = Flask(__name__,static_folder='../public/static')
 engfordev.config["MONGO_HOST"] = config.MONGO_HOST
 engfordev.config["MONGO_PORT"] = config.MONGO_PORT
 engfordev.config["MONGO_DBNAME"] = config.MONGO_DBNAME
-#engfordev.config["MONGO_USERNAME"] = config.MONGO_USERNAME
-#engfordev.config["MONGO_PASSWORD"] = config.MONGO_PASSWORD
+engfordev.config["MONGO_USERNAME"] = config.MONGO_USERNAME
+engfordev.config["MONGO_PASSWORD"] = config.MONGO_PASSWORD
 mongo = PyMongo(engfordev)
 
 import views
